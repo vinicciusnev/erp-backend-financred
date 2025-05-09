91ee2391cf26d74125d76e1db698c339e6b51daa
vinicciusnev <xxnashflows@hotmail.com>
2025-05-09 19:29:06 -0300

    fix: alterando cors, para aceitar requisições da porta 8081

--- src/main/java/com/financred/financred/config/SecurityConfig.java
+++ src/main/java/com/financred/financred/config/SecurityConfig.java
@@ -56,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:443", "http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:443", "http://localhost:5173", "http://localhost:8081"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
