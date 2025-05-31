fcd7d0f8499379e66d85383ef90b6673b53fc4ea
vinicciusnev <xxnashflows@hotmail.com>
2025-05-30 21:26:10 -0300

    refactor: Refatorando api

--- src/main/java/com/financred/financred/controller/dto/response/RegisterResponseDTO.java
+++ src/main/java/com/financred/financred/controller/dto/response/RegisterResponseDTO.java
@@ -1,3 +1,3 @@
-package com.financred.financred.dto.reponse;
+package com.financred.financred.controller.dto.response;
 
 public record RegisterResponseDTO(String token, String role, Long idCliente) { }
